f6b1d65bbde1cc0bb73b7a235f2b1938e308b991
Vipin Singh <svipin174@gmail.com>
2024-08-04 14:00:10 +0530

    updating validation text

--- src/schema/form-schema.ts
+++ src/schema/form-schema.ts
@@ -5,7 +5,7 @@ export const registerFormSchema = z.object({
     message: 'Name must be at least 2 characters.',
   }),
   email: z.string().email({
-    message: 'Email is required',
+    message: 'Email must be at least 3 characters including @.',
   }),
   username: z.string().min(2, {
     message: 'Username must be at least 2 characters.',
