3cc57c412233327c5d2e7e32885d5dde27db0480
Vipin Singh <svipin174@gmail.com>
2024-08-04 16:08:36 +0530

    hash the password

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -2,17 +2,19 @@
 import { createServerAction } from 'zsa';
 import { registerFormSchema } from '@/schema/form-schema';
 import { prisma } from '@/config/prisma';
+import bcrypt from 'bcrypt';
 
 export const registerAction = createServerAction()
   .input(registerFormSchema)
   .handler(async ({ input }) => {
     console.log(input);
+    const hashPassword = await bcrypt.hash(input.password, 10);
     const user = await prisma.user.create({
       data: {
         name: input.name,
         email: input.email,
         username: input.username,
-        password: input.password,
+        password: hashPassword,
       },
     });
     console.log('Data send to mongo db', user);
