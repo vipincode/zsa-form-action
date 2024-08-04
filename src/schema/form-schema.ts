c976cbfb2d71ef1ad0f749a9a019e26e529b3699
Vipin Singh <svipin174@gmail.com>
2024-08-04 13:55:49 +0530

    basic setup and adding form validation

---
+++ src/schema/form-schema.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+export const registerFormSchema = z.object({
+  name: z.string().min(2, {
+    message: 'Name must be at least 2 characters.',
+  }),
+  email: z.string().email({
+    message: 'Email is required',
+  }),
+  username: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }),
+  password: z.string().min(6, {
+    message: 'Minimum 6 characters required',
+  }),
+});
